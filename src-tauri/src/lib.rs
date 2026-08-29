d3d5dd4f0155f8b9442f0dc16d88160ef7b960b9
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-29 07:27:51 +0300

    feat(pdf): register scanned page OCR command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ macro_rules! openmind_generate_handler {
         $crate::tauri_crate::generate_handler![
             $($command),*,
             transcribe_audio,
+            ocr_pdf_pages,
             send_multimodal_chat_message,
             regenerate_multimodal_message,
             artifact_media_data_url,
@@ -20,11 +21,14 @@ mod tauri {
 }
 
 mod multimodal;
+mod pdf_ocr;
 mod speech_runtime;
+mod vision_batch;
 
 pub(crate) use multimodal::{
     artifact_media_data_url, create_soundscape_artifact, regenerate_multimodal_message,
     send_multimodal_chat_message, transcribe_audio,
 };
+pub(crate) use pdf_ocr::ocr_pdf_pages;
 
 include!("lib_legacy.rs");
