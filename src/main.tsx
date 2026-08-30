03b5ce2a1d6999ec241d5b709a01a207a24c1bd8
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-30 23:26:16 +0300

    feat: load mobile responsive styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./multimodal.css";
 import "./projects.css";
 import "./work.css";
+import "./mobile.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
