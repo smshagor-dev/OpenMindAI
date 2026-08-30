2eba3389580ece1a1ae93b039b27eb0be35bfc7b
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-31 01:02:00 +0300

    feat(mobile): bootstrap native platform state in frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { bootstrapPlatformDataset } from "./lib/platform";
 import "./styles.css";
 import "./multimodal.css";
 import "./projects.css";
 import "./work.css";
 import "./mobile.css";
+import "./mobile-platform.css";
+
+bootstrapPlatformDataset();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
