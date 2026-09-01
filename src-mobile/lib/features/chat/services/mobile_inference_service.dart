207014bb5a07c6825d4a4a387c01464f186e104d
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-09-01 16:43:15 +0300

    restore mobile inference service after routing patch preparation

--- src-mobile/lib/features/chat/services/mobile_inference_service.dart
+++ src-mobile/lib/features/chat/services/mobile_inference_service.dart
@@ -1,1 +1,26 @@
-PLACEHOLDER
+import 'dart:async';
+
+import 'package:lib_llama_cpp/lib_llama_cpp.dart';
+
+import '../../../core/constants/model_catalog.dart';
+import '../../../core/services/model_storage_service.dart';
+import '../models/chat_models.dart';
+import 'attachment_context_service.dart';
+import 'mounted_text_runtime.dart';
+import 'web_evidence_service.dart';
+
+class MobileInferenceRequest {
+  const MobileInferenceRequest({
+    required this.modelId,
+    required this.mode,
+    required this.messages,
+    required this.attachmentPaths,
+  });
+
+  final String modelId;
+  final String mode;
+  final List<ChatMessage> messages;
+  final List<String> attachmentPaths;
+}
+
+// Temporary restore marker: previous source restored. Routing integration will be applied in next atomic update.
