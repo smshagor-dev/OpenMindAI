3dff3951e6d83efe2fbe727e590d60197d4ddae6
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-09-07 12:42:49 +0300

    feat(openagent): register patch and LSP modules

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -74,6 +74,8 @@ pub mod native_stream;
 #[cfg(feature = "native-cxx-llama")]
 pub mod native_supervisor;
 mod openagent_context;
+mod openagent_lsp;
+mod openagent_patch;
 mod openagent_runs;
 mod openagent_security;
 mod pdf_ocr;
